6f5ce9ab5f2c1cd4664541a711f4aad99c45b56e
yj-603-07 <yj-603-07@DESKTOP-GBF4003>
2020-10-07 11:11:47 +0900

    2233

--- src/d0910/Helloworld.java
+++ src/d0910/Helloworld.java
@@ -5,6 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World");
 		System.out.println("Hello, World");
+		System.out.println("헬로, 월드");
 	}
 
 }
